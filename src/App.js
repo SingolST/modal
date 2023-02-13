f0c99aeb67a4701862ea196580e05c7f9ab5bae9
Stas <singol.draft@gmail.com>
2023-02-13 10:47:59 +0500

    second commit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import React, { useState } from 'react';
-import Modal from './components/Modal/Modal';
+import Modal from './components/Modal/Modal'
 import './App.css';
 
 function App() {
-
   const [open, setOpen] = useState(false);
 
   return (
     <div className="App">
       <button onClick={() => setOpen(true)} className="open-modal-btn">✨ Открыть окно</button>
-      <Modal open={open} setOpen={setOpen}>
-        <img src="https://media2.giphy.com/media/xT0xeJpnrWC4XWblEk/giphy.gif" />
-        <h3>Это модальное окно</h3>
-      </Modal>
+      <Modal open={open} setOpen={setOpen} />
     </div>
-  );
+  ); 
 }
 
 export default App;
